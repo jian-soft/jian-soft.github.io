2e09647e4e242b8825efb1d7fbe745dba7688c54
jiansoft <jiansoft@yeah.net>
2021-12-26 01:04:24 +0800

    部署之前先编译

--- d_hks.py
+++ d_hks.py
@@ -8,13 +8,16 @@ import os
 gitee_path = "/var/www/site/_site"
 site_path = "_site/*"
 print("*******************************")
-print("copy to gitee path "+gitee_path)
+print("build...")
+os.system("bundle exec jekyll build")
+os.system("rm _site/d_hks.py _site/serv.sh")
+
+
 print("*******************************")
 print("copy to gitee path "+gitee_path)
 os.system("cp -rv "+site_path+" "+gitee_path)
+
 print("*******************************")
 print("git st and git cm and git push")
-print("*******************************")
-print("copy to gitee path "+gitee_path)
 #os.system("cd "+gitee_path+"; git st; git add .; git cm -m 'update'; git push")
 os.system("cd "+gitee_path+"; git st")
